e6da9cfb9b65a834577600690c318fa24d9d1d8f
annysena <annysena.as44@gmail.com>
2021-07-28 13:40:20 -0300

    API Rest atualizada em 28-07-2021

--- blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -6,11 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity //Create Table
 @Table(name = "tb_postagens") // tb_postagens(
@@ -37,6 +40,10 @@ public class Postagem {
 	 */
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date data = new java.sql.Date(System.currentTimeMillis()); // Date Timestamp()
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 
 	/**
 	 * 
@@ -74,5 +81,12 @@ public class Postagem {
 	public void setData(Date data) {
 		this.data = data;
 	}
+	
+	public Tema getTema() {
+		return this.tema;
+	}
 
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 }
